816414fbb8a547d8234d7d2a10d996addbd44dd7
rendrabimaa <rendrabimaa@gmail.com>
2023-08-30 09:35:03 +0700

    implementasi styling

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,14 +1,19 @@
+import { useRouter } from "next/router";
 import Navbar from "../Navbar"
 
 type AppShellProps = {
     children: React.ReactNode;
 }
 
+const disableNavbar = ["/auth/login", "/auth/register"]
+
 const AppShell = (props: AppShellProps) => {
     const { children } = props;
+    const { pathname } = useRouter()
+    console.log(pathname)
     return (
         <main>
-            <Navbar />
+            {!disableNavbar.includes(pathname) && <Navbar /> }
             {children}
         </main>
     )
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,19 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import LoginViews from "@/views/auth/login";
 
-const LoginPage = () => {
-    const { push } = useRouter()
-    const handlerLogin = () => {
-        push('/product')
-    }
+const LoginPage = () => {    
     return (
-        <div>
-            <h1>Login Page</h1>
-            <button onClick={handlerLogin}>Login</button>
-            <p>
-                Belum punya akun? registrasi <Link href={"/auth/register"}>disini</Link>
-            </p>
-        </div>
+        <>
+            <LoginViews />
+        </>
     )
 }
 
